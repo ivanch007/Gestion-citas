34079d1f7d656e90335a3a1e9d96b6da5301a101
Ivan Ocampo <ivanocampo07@gmail.com>
2023-11-29 17:50:11 -0500

    Se implementa la función para mostrar el mensaje de la selección de los formularios

--- gestion citas/gestioncitas.js
+++ gestion citas/gestioncitas.js
@@ -1,28 +1,36 @@
 function selecDepartamentoMedico(){
     let areaMedica = document.getElementById('areamedica')
     let eleccionArea = areaMedica.value
-
-    document.getElementById('selecDepartamento').innerText = `Ha seleccionado el área ${eleccionArea}. `
 }
 
 function selecProfesionales(){
     let profesionales = document.getElementById('selecprofesional')
     let eleccionProfesional = profesionales.value
 
-    document.getElementById('selecPro').innerText = `El profesionales seleccionado es ${eleccionProfesional}`
 
 }
 
 
-    let fecha = document.getElementById('selefecha')
-    let mostrarFecha = document.getElementById('spanFecha')
-    window.addEventListener('load', function(){
+let fecha = document.getElementById('selefecha')
+let mostrarFecha = document.getElementById('spanFecha')
+window.addEventListener('load', function(){
         
-        fecha.addEventListener('change', function(){
-            mostrarFecha.innerText = `Le fecha seleccionada es ${this.value}`
-        })
-    })
+    
+})
+
+function mostrarMensaje() {
+    let areaMedica = document.getElementById('areamedica').value;
+    let profesional = document.getElementById('selecprofesional').value;
+    let fecha = document.getElementById('selefecha').value;
+    let mensaje = document.getElementById('mensaje');
+
+    if (areaMedica === "Selecciona una opción" || profesional === "Selecciona al profesional" || fecha === "") {
+        mensaje.innerText = "Por favor, completa todos los campos antes de seleccionar la cita.";
+    } else {
+        mensaje.innerText = `Tu cita médica con el área ${areaMedica} y el profesional ${profesional} en la fecha ${fecha} quedó asignada.`;
+    }
+}
 
 
 
-//this.value.document.getElementById('spanFecha').innerText = `Le fecha seleccionada es ${fecha}`
+
