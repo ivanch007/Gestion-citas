de651d028fabb88bb1c76851492331465da51ceb
Ivan Ocampo <ivanocampo07@gmail.com>
2023-12-01 11:23:03 -0500

    Se corrigen los link para acceder a la hoja de estilos CSS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function loguear(){
     let user = document.getElementById('usuario').value
     let password = document.getElementById('contraseña').value
 
-    if(user == 12345 && password == 7894){
+    if(user == 1234 && password == 1234){
         window.location = "./inicio/inicio.html" 
     }
     else{
